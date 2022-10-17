8b41ee21b126fbd4b4cc9365ec1d72604bcb7d9b
willywin99 <willywinsianturi@gmail.com>
2022-10-17 20:38:12 +0700

    feat: server.js Understanding and Working with Routes

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.get('/', function (req, res) {
   res.send('<h1>Hello, World!</h1>');
 });
 
+app.get('/contact', function (req, res) {
+  res.send('Contact me at: willywinsianturi@gmail.com');
+});
+
+app.get('/about', function (req, res) {
+  res.send('My name is Willy and I love coffee and music.');
+});
+
+app.get('/hobbies', function (req, res) {
+  res.send('<ul><li>Coffee</li><li>Code</li><li>Music</li></ul>');
+});
+
 app.listen(3000, function () {
   console.log('Server started on port 3000');
 });
